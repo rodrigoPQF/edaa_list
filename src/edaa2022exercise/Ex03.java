2960b2d6115d0a1e2b341d6b956c9b724481663e
Rodrigo Pereira Queiroz <62712725+rodrigoPQF@users.noreply.github.com>
2022-09-03 20:00:45 -0300

    Update Ex03.java

--- src/edaa2022exercise/Ex03.java
+++ src/edaa2022exercise/Ex03.java
@@ -3,14 +3,7 @@ package edaa2022exercise;
 import java.util.Arrays;
 
 public class Ex03 {
-    /*
-   Faça um programa que preencha um vetor com os modelos de cinco carros (exemplos de modelos: Fusca, Gol, Vectra etc).
-    Carregue outro vetor com o consumo desses carros, isto é, quantos quilômetros cada um deles faz com um litro de combustível.
-     Calcule e mostre:
-        o modelo de carro mais econômico;
-        e quantos litros de combustível cada um dos carros cadastrados consome para percorrer uma distância de 1.000 km
 
-     */
     public static void main(String[] args){
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
         int[] gas = new int[]{22,12,13,14};
